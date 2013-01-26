122b93215d4f918bd844c00513ee88182139b2c8
Max Nowack <nov@zlies.de>
2013-01-27 00:38:05 +0100

    added another sample project with a chart

---
+++ MN.Cinemaxx.Reservations.SampleForm/SampleForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MN.Cinemaxx.Reservations.SampleForm
+{
+    using System;
+
+    partial class SampleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dtpDay = new System.Windows.Forms.DateTimePicker();
+            this.cmbCinema = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtpDay
+            // 
+            this.dtpDay.Location = new System.Drawing.Point(142, 13);
+            this.dtpDay.Name = "dtpDay";
+            this.dtpDay.Size = new System.Drawing.Size(200, 20);
+            this.dtpDay.TabIndex = 1;
+            // 
+            // cmbCinema
+            // 
+            this.cmbCinema.FormattingEnabled = true;
+            this.cmbCinema.Items.AddRange(new object[] {
+            "8"});
+            this.cmbCinema.Location = new System.Drawing.Point(12, 12);
+            this.cmbCinema.Name = "cmbCinema";
+            this.cmbCinema.Size = new System.Drawing.Size(121, 21);
+            this.cmbCinema.TabIndex = 2;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.Location = new System.Drawing.Point(749, 15);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 20);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.AxisX.IsStartedFromZero = false;
+            chartArea1.AxisX.MajorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.AxisX.MinorGrid.Interval = double.NaN;
+            chartArea1.CursorX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 39);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(812, 346);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // SampleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 397);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cmbCinema);
+            this.Controls.Add(this.dtpDay);
+            this.Name = "SampleForm";
+            this.Text = "CinemaxX Besucher";
+            this.Load += new System.EventHandler(this.SampleForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpDay;
+        private System.Windows.Forms.ComboBox cmbCinema;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ MN.Cinemaxx.Reservations.SampleForm/SampleForm.cs
@@ -0,0 +1,100 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Form1.cs" company="Max Nowack">
+//   copyright notice
+// </copyright>
+// <summary>
+//   Defines the Form1 type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MN.Cinemaxx.Reservations.SampleForm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Windows.Forms;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Windows.Forms.DataVisualization.Charting;
+    using System.Xml.Serialization;
+
+    using CristiPotlog.ChartControl;
+
+    /// <summary>
+    /// The sample form.
+    /// </summary>
+    public partial class SampleForm : Form
+    {
+        private static string CacheFile { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SampleForm"/> class.
+        /// </summary>
+        public SampleForm()
+        {
+            this.InitializeComponent();
+            CacheFile = Directory.GetCurrentDirectory() + "\\data.xml"; //Path.GetTempFileName();
+        }
+
+        /// <summary>
+        /// The btn refresh_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            var m = new Cinema(int.Parse("8")).GetMovies(this.dtpDay.Value);
+            var serializer = new XmlSerializer(typeof(List<Movie>));
+            var stream = new FileStream(CacheFile, FileMode.Truncate);
+            serializer.Serialize(stream, m);
+            stream.Close();
+
+            this.GenerateChart();
+        }
+
+        /// <summary>
+        /// The generate chart.
+        /// </summary>
+        private void GenerateChart()
+        {
+            var serializer = new XmlSerializer(typeof(List<Movie>));
+            var m = new List<Movie>();
+
+            var stream = new FileStream(CacheFile, FileMode.OpenOrCreate);
+            if (stream.Length > 0)
+            {
+                m = (List<Movie>)serializer.Deserialize(stream);
+                stream.Close();
+            }
+            else
+            {
+                stream.Close();
+                this.btnRefresh_Click(new object(), new EventArgs());
+            }
+            
+            var times = m.Where(x => x.Name != string.Empty && x.Time >= this.dtpDay.Value)
+                         .GroupBy(x => x.Time)
+                         .Select(mov => new { Time = mov.Key, Reservations = mov.Sum(x => x.Reservations) })
+                 .OrderBy(x => x.Time);
+
+            var s = this.chart1.Series.FirstOrDefault();
+            s.Points.Clear();
+            foreach (var time in times)
+            {
+                var dp = new DataPoint();
+                dp.SetValueXY(time.Time, time.Reservations);
+                s.Points.Add(dp);
+            }
+        }
+
+        private void SampleForm_Load(object sender, EventArgs e)
+        {
+            this.GenerateChart();
+        }
+    }
+}
