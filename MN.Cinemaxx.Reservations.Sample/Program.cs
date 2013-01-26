2e0eea861ac83aae7cbfde099f0c314a2d531432
Max Nowack <nov@zlies.de>
2013-01-26 14:58:26 +0100

    added sample projects

---
+++ MN.Cinemaxx.Reservations.Sample/Program.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Max Nowack">
+//   copyright notice
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MN.Cinemaxx.Reservations.Sample
+{
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
