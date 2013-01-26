05160c09b4310c0e8ad90307199590fdadf58197
Max Nowack <nov@zlies.de>
2013-01-26 14:55:43 +0100

    added basic classes

---
+++ MN.Cinemaxx.Reservations/Cinema.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Cinema.cs" company="Max Nowack">
+//   copyright notice
+// </copyright>
+// <summary>
+//   Defines the Cinema type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MN.Cinemaxx.Reservations
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The cinema.
+    /// </summary>
+    public class Cinema
+    {
+        /// <summary>
+        /// The cinema id.
+        /// </summary>
+        private int cinemaId;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cinema"/> class.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        public Cinema(int id)
+        {
+            this.cinemaId = id;
+        }
+
+        /// <summary>
+        /// The get movies.
+        /// </summary>
+        /// <param name="week">
+        /// The week.
+        /// </param>
+        /// <returns>
+        /// The <see cref="List"/>.
+        /// </returns>
+        public List<Movie> GetMovies(DateTime week)
+        {
+            var retVal = new List<Movie>();
+            return retVal;
+        }
+    }
+}
--- MN.Cinemaxx.Reservations/Movie.cs
+++ MN.Cinemaxx.Reservations/Movie.cs
@@ -1,13 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Movie.cs" company="Max Nowack">
+//   copyright notice
+// </copyright>
+// <summary>
+//   Defines the Movie type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace MN.Cinemaxx.Reservations
 {
+    using System;
+
+    /// <summary>
+    /// The movie.
+    /// </summary>
     public class Movie
     {
+        /// <summary>
+        /// The id.
+        /// </summary>
+        private readonly string id;
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time.
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// Gets the reservations.
+        /// </summary>
+        public long Reservations 
+        { 
+            get { return GetReservations(this.id); }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Movie"/> class.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        public Movie(string id)
+        {
+            this.id = id;
+        }
+
+        /// <summary>
+        /// The get reservations.
+        /// </summary>
+        /// <param name="movieId">
+        /// The movie Id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="long"/>.
+        /// </returns>
+        private static long GetReservations(string movieId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
